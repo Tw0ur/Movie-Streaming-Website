0e753ef32615cc0a9e6c0532e1151f566d3075a3
Tw0ur <twour@inbox.ru>
2023-09-06 20:16:00 +0600

    My commit comments

---
+++ app/auth/auth.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Auth = () => {
+  return (
+    <div className='w-full '>
+      <div className='w-full h-screen bg-[url("/login/landing.jpg")]' >
+ 
+      </div>
+    </div>
+  )
+}
+
+export default Auth
